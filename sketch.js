6a7b4a7fdfee20c484c3b8d80421ac84ceb1b2f9
jdator <56280952+jdator@users.noreply.github.com>
2021-05-23 17:26:10 -0700

    simpledraw

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+
+  strokeWeight(15);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed){
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
+}
+
+function keyTyped(){
+
+  if (key === 's'){
+    saveCanvas('fileName', 'png');
+  } else if (key === 'c') {
+
+    clear();
+  }
 }
